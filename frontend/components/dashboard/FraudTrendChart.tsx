ed0d8f62c61afc17fa69e912f1ea640513ef1b26
Shaik.Abdul Sammed <samm41236@gmail.com>
2026-01-30 08:44:20 +0530

    add

--- frontend/components/dashboard/FraudTrendChart.tsx
+++ frontend/components/dashboard/FraudTrendChart.tsx
@@ -17,7 +17,7 @@ export function FraudTrendChart() {
     return (
         <Card className="col-span-4">
             <CardHeader>
-                <CardTitle>Analysis Trends (7 Days)</CardTitle>
+                <CardTitle>Analysis Trend (7 Days)</CardTitle>
             </CardHeader>
             <CardContent className="pl-2">
                 <ResponsiveContainer width="100%" height={350}>
